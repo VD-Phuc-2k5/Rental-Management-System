a7e546756011dfe8bb51ebb67056ea48498d4540
KrySoSad <duythuc070205@gmail.com>
2026-02-27 15:07:49 +0700

    refactor(home-screen): use StatefulWidget and integrate Search and Filter components

--- client/lib/screens/home-screen/components/body.dart
+++ client/lib/screens/home-screen/components/body.dart
@@ -1,16 +1,37 @@
+import 'package:app/screens/home-screen/components/filter_price.dart';
+import 'package:app/screens/home-screen/components/search_filter.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreenBody extends StatelessWidget {
+class HomeScreenBody extends StatefulWidget {
   const HomeScreenBody({super.key});
+
+  @override
+  State<HomeScreenBody> createState() => _HomeScreenBodyState();
+}
+
+class _HomeScreenBodyState extends State<HomeScreenBody> {
+  int _selectedFilterIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      child: const Center(
-        child: Text("Home Screen Body"),
-      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SearchFilter(),
+          FilterPrice(
+            currentIndex: _selectedFilterIndex,
+            onTap: (index) {
+              setState(() {
+                _selectedFilterIndex = index;
+              });
+              // TODO: handle lọc theo giá
+            },
+          ),
+        ]
+      )
     );
   }
 }
