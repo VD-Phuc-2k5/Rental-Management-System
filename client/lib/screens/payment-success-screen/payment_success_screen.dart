3b6b01ded8e21859397d940229148603aee0f4d8
ErikDev5 <voducphuc572@gmail.com>
2026-02-28 00:44:23 +0700

    refactor(payment-success-screen): remove unused import

--- client/lib/screens/payment-success-screen/payment_success_screen.dart
+++ client/lib/screens/payment-success-screen/payment_success_screen.dart
@@ -1,7 +1,6 @@
 import 'package:app/screens/payment-success-screen/components/body.dart';
 import 'package:app/core/widgets/common_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:app/core/constants.dart';
 
 class PaymentSuccessScreen extends StatelessWidget {
   final int price;
