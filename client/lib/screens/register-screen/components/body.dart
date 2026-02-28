627a7d3005d52a120ce8c06de0ff75a4822190f2
KrySoSad <duythuc070205@gmail.com>
2026-02-28 18:42:40 +0700

    refactor(register-screen): RegisterBody to include AppBar and improve layout structure

--- client/lib/screens/register-screen/components/body.dart
+++ client/lib/screens/register-screen/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:app/core/constants.dart';
 import 'package:app/screens/register-screen/components/register_form.dart';
 import 'package:flutter/material.dart';
 import 'package:app/screens/register-screen/components/register_logo.dart';
@@ -7,35 +8,60 @@ class RegisterBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                child: Column(
-                  children: [
-                    const RegisterLogo(),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Chào mừng đến với NhàTrọ+',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'Inter',
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      appBar: AppBar(
+        backgroundColor: AppColors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: AppColors.blue950),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        titleSpacing: 0,
+        title: const Padding(
+          padding: EdgeInsets.only(right: 56.0),
+          child: Center(
+            child: Text(
+              "Tạo tài khoản",
+              style: TextStyle(
+                color: AppColors.blue950,
+                fontFamily: "Inter",
+                fontWeight: FontWeight.w800,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: Column(
+                    children: [
+                      const RegisterLogo(),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Chào mừng đến với NhàTrọ+',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Inter',
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                child: const RegisterForm(),
-              ),
-            ],
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: const RegisterForm(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- client/lib/screens/register-screen/register_screen.dart
+++ client/lib/screens/register-screen/register_screen.dart
@@ -1,4 +1,3 @@
-import 'package:app/core/constants.dart';
 import 'package:app/screens/register-screen/components/body.dart';
 import 'package:flutter/material.dart';
 
@@ -7,31 +6,6 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.white,
-      appBar: AppBar(
-        backgroundColor: AppColors.white,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: AppColors.blue950),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        titleSpacing: 0,
-        title: const Padding(
-          padding: EdgeInsets.only(right: 56.0),
-          child: Center(
-            child: Text(
-              "Tạo tài khoản",
-              style: TextStyle(
-                color: AppColors.blue950,
-                fontFamily: "Inter",
-                fontWeight: FontWeight.w800,
-                fontSize: 20,
-              ),
-            ),
-          ),
-        ),
-      ),
-      body: const RegisterBody(),
-    );
+    return const RegisterBody();
   }
 }
