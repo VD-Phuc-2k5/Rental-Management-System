b99b1fdeb3c6311c390d51d0a34a563a08387825
KrySoSad <duythuc070205@gmail.com>
2026-02-28 20:24:14 +0700

    feat(room-detail-screen): implement RoomDetailScreen with body and booking confirmation components

---
+++ client/lib/screens/room-detail-screen/room_detail_screen.dart
@@ -0,0 +1,26 @@
+import 'package:app/core/constants.dart';
+import 'package:app/screens/room-detail-screen/components/body.dart';
+import 'package:app/screens/room-detail-screen/components/comfirm_book_room.dart';
+import 'package:flutter/material.dart';
+
+class RoomDetailScreen extends StatelessWidget {
+  const RoomDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: RoomDetailBody(),
+        ),
+      ),
+      bottomNavigationBar: ComfirmBookRoom(
+        price: 2800000,
+        onPressed: () {
+          // TODO: Navigate to booking screen
+        },
+      ),
+    );
+  }
+}
