25049ed69f34419f3fc6e0877a419d89afa23c83
KrySoSad <duythuc070205@gmail.com>
2026-03-02 02:39:50 +0700

    feat: enhance OTP input fields with improved focus handling and visual feedback

--- client/lib/screens/forgot-password-screen/components/input-otp/otp_input_fields.dart
+++ client/lib/screens/forgot-password-screen/components/input-otp/otp_input_fields.dart
@@ -21,25 +21,34 @@ class OtpInputFields extends StatefulWidget {
 class _OtpInputFieldsState extends State<OtpInputFields> {
   late List<TextEditingController> _controllers;
   late List<FocusNode> _focusNodes;
+  // Index of the field currently accepting input
+  int _activeIndex = 0;
 
   @override
   void initState() {
     super.initState();
     _controllers = List.generate(widget.length, (_) => TextEditingController());
     _focusNodes = List.generate(widget.length, (_) => FocusNode());
+    // Auto-focus the first field
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) _focusNodes[0].requestFocus();
+    });
   }
 
   @override
   void dispose() {
-    for (final c in _controllers) c.dispose();
-    for (final f in _focusNodes) f.dispose();
+    for (final c in _controllers) {
+      c.dispose();
+    }
+    for (final f in _focusNodes) {
+      f.dispose();
+    }
     super.dispose();
   }
 
   void _onChanged(String value, int index) {
-    if (value.isEmpty && index > 0) {
-      _focusNodes[index - 1].requestFocus();
-    } else if (value.length == 1 && index < widget.length - 1) {
+    if (value.length == 1 && index < widget.length - 1) {
+      setState(() => _activeIndex = index + 1);
       _focusNodes[index + 1].requestFocus();
     }
 
@@ -62,53 +71,61 @@ class _OtpInputFieldsState extends State<OtpInputFields> {
           child: SizedBox(
             width: 44,
             height: 44,
-            child: Focus(
-              onKeyEvent: (node, event) {
-                if (event is KeyDownEvent &&
-                    event.logicalKey == LogicalKeyboardKey.backspace &&
-                    _controllers[index].text.isEmpty &&
-                    index > 0) {
-                  _focusNodes[index - 1].requestFocus();
-                  _controllers[index - 1].clear();
-                  return KeyEventResult.handled;
-                }
-                return KeyEventResult.ignored;
-              },
-              child: TextFormField(
-                controller: _controllers[index],
-                focusNode: _focusNodes[index],
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  LengthLimitingTextInputFormatter(1),
-                ],
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  fontFamily: 'Noto Sans',
-                  color: AppColors.blue950,
-                ),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: EdgeInsets.zero,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(
-                      color: AppColors.slate200,
-                      width: 2,
+            child: IgnorePointer(
+              ignoring: index > _activeIndex,
+              child: Focus(
+                onKeyEvent: (node, event) {
+                  if (event is KeyDownEvent &&
+                      event.logicalKey == LogicalKeyboardKey.backspace &&
+                      _controllers[index].text.isEmpty &&
+                      index > 0) {
+                    _focusNodes[index - 1].requestFocus();
+                    _controllers[index - 1].clear();
+                    setState(() => _activeIndex = index - 1);
+                    return KeyEventResult.handled;
+                  }
+                  return KeyEventResult.ignored;
+                },
+                child: TextFormField(
+                  controller: _controllers[index],
+                  focusNode: _focusNodes[index],
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(1),
+                  ],
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Noto Sans',
+                    color: AppColors.blue950,
+                  ),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: index > _activeIndex
+                        ? AppColors.slate100
+                        : Colors.white,
+                    contentPadding: EdgeInsets.zero,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(
+                        color: index > _activeIndex
+                            ? AppColors.slate200
+                            : AppColors.slate300,
+                        width: 2,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: AppColors.blue700,
+                        width: 2,
+                      ),
                     ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(
-                      color: AppColors.blue700,
-                      width: 2,
-                    ),
-                  ),
+                  onChanged: (value) => _onChanged(value, index),
                 ),
-                onChanged: (value) => _onChanged(value, index),
               ),
             ),
           ),
