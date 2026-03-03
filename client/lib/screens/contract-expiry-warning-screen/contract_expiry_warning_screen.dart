d98143daac79df930364747e7e0fcb2bedc264a0
ErikDev5 <voducphuc572@gmail.com>
2026-03-03 19:25:30 +0700

    refactor(contract-expiry-warning-screen): rename class and update icon layout

--- client/lib/screens/contract-expiry-warning-screen/contract_expiry_warning_screen.dart
+++ client/lib/screens/contract-expiry-warning-screen/contract_expiry_warning_screen.dart
@@ -2,10 +2,10 @@ import 'package:app/core/widgets/common_appbar.dart';
 import 'package:app/screens/contract-expiry-warning-screen/components/body.dart';
 import 'package:flutter/material.dart';
 
-class LandlordContractExpiryWarningScreen extends StatelessWidget {
+class ContractExpiryWarningScreen extends StatelessWidget {
   // TO DO: Add contract parameter when contract model is available
   // final Contract contract;
-  const LandlordContractExpiryWarningScreen({super.key});
+  const ContractExpiryWarningScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
