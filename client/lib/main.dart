105ebf3bc046eea3f8785dd9c935c892ac189ec0
ErikDev5 <voducphuc572@gmail.com>
2026-03-03 12:13:15 +0700

    refactor(main): switch home screen to HomeScreen component

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:app/screens/home-screen/home_screen.dart';
-import 'package:app/screens/landlord-maintenance-requests-screen/landlord_maintenance_requests_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:app/core/constants.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -18,7 +17,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: AppColors.white),
-      home: LandlordMaintenanceRequestsScreen(),
+      home: const HomeScreen(),
     );
   }
 }
