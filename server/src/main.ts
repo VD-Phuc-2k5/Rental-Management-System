c160fc3fd77f776f508ddfa29d55f849b11408bd
KrySoSad <duythuc070205@gmail.com>
2026-03-27 02:35:14 +0700

    feat: add global interceptors and filters for HTTP response handling

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { HttpResponseInterceptor } from './shared/common/interceptors/HttpResponse.interceptor';
+import { HttpExceptionFilter } from './shared/common/filter/HttpException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalInterceptors(new HttpResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
